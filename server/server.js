793df92381470aaa13289a1626d0c3a4feba60e4
rubensseva <r.sevaldson@gmail.com>
2019-09-30 20:38:50 +0200

    Issue #1 - Make server able to handle POST requests
    Other notable changes:
      * Add example express route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const bodyParser = require("body-parser");
+
+const example = require('./example/example.js');
+
 const app = express()
 const port = 3000
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/example', example);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 

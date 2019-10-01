2f38584ce948e2df3737be58530f0589f8c90b36
Zmrihawy <zmrihawy@stud.ntnu.no>
2019-10-01 18:19:39 +0200

    #16 issue Create mainpage

---
+++ client/src/components/MainPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+class MainPage extends Component {
+
+
+render() {
+    return (
+      <div className="MainPage">
+
+      </div>
+    );
+  }
+}
+
+
+
+export default MainPage;

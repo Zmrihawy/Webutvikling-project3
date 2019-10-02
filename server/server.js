deeacd0fa4042aac5f1ae3c9c9bee390aaf21ad4
rubensseva <r.sevaldson@gmail.com>
2019-10-02 14:47:24 +0200

    Issue #4 - Basic connection to mongodb using mongoose

--- server/server.js
+++ server/server.js
@@ -8,6 +8,19 @@ const port = 3000
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+// getting-started.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://AppUser:MilkShake@it2810-30.idi.ntnu.no/db01', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Succesfully connected to mongodb server");
+});
+
+
+
 app.use('/example', example);
 
 app.get('/', (req, res) => res.send('Hello World!'))

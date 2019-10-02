61a6dec8669468217bb83f7f02bfb1e72699561a
rubensseva <r.sevaldson@gmail.com>
2019-10-02 16:33:26 +0200

    Issue #4, #18, #19 - Create basic endpoints for components
    Notable changes:
      * Created mongoose schema for components
      * Added post endpoint to create components
      * Added get endpoint to get all components

---
+++ server/endpoints/component/componentRoute.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const componentModel = require('./componentModel.js');
+const router = express.Router();
+
+// On every hit, log the time
+router.use(function timeLog (req, res, next) {
+  console.log('Components endpoint hit:', new Date(Date.now()).toString());
+  next();
+})
+
+// Get request to get all components
+router.get('/', function (req, res) {
+  componentModel.find()
+    .then(components => res.send(components))
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(err);
+    })
+})
+
+// Post request to create a new component
+router.post('/', function (req, res) {
+  let component = new componentModel({
+    name: req.body.name, 
+    category: req.body.category, 
+    description: req.body.description, 
+    producer: req.body.producer,
+    specs: JSON.parse(req.body.specs)
+  }) 
+  component.save()
+    .then(updatedComponent => (
+      res.status(201).send(JSON.stringify(updatedComponent))))
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(err);
+    })
+})
+
+// Get request to get some simple info
+router.get('/about', function (req, res) {
+  res.send('This is the endpoint for components');
+})
+
+//TODO: Add get request to get component by id
+//TODO: Add put and/or patch request(s) to update components by id and values
+//TODO: Add delete request to delete a component
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require("body-parser");
 
 const example = require('./example/example.js');
+const componentRoute = require('./endpoints/component/componentRoute.js');
 
 const app = express()
 const port = 3000
@@ -22,7 +23,10 @@ db.once('open', function() {
 
 
 app.use('/example', example);
+app.use('/component', componentRoute);
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

b0b77e44594d41d9ec63c8e3f5ad46516d3a1816
rubensseva <r.sevaldson@gmail.com>
2019-10-06 13:27:29 +0200

    Issue #3 - Improve list based component

--- client/src/components/ListView.js
+++ client/src/components/ListView.js
@@ -5,15 +5,43 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
 import Divider from '@material-ui/core/Divider';
 import InboxIcon from '@material-ui/icons/Inbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
+
+import Collapse from '@material-ui/core/Collapse';
+import SendIcon from '@material-ui/icons/Send';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
+
+
 import { connect } from 'react-redux';
 
 import { getComponents } from '../redux/actions/componentActions';
 
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    // maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
+
 const ListView = (props) => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   console.log(props);
 
   useEffect(() => {
@@ -21,19 +49,49 @@ const ListView = (props) => {
     props.getComponents();
   }, [JSON.stringify(props.components)])
 
+
+  
   const mappedComponents = props.components.map(component => (
-    <div key={component._id}>
-      <p>
-        {component.name}
-      </p>
-    </div>
+    <React.Fragment key={component._id}>
+      <ListItem button onClick={handleClick}>
+        <ListItemText primary={component.name}/>
+        <ListItemText primary={component.category}/>
+        <ListItemText primary={component.producer}/>
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <ListItem>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            { component.specs.map(spec => (
+              <ListItem className={classes.nested}>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary={spec.name}/>
+                <ListItemText primary={spec.value} />
+                <ListItemText primary={spec.description} />
+              </ListItem>
+            ))
+            }
+          </List>
+        </Collapse>
+      </ListItem>
+  </React.Fragment>
   ))
 
   return (
-    <div>
-      ListView
+      <List
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={
+        <ListSubheader component="div" id="nested-list-subheader">
+          Components
+        </ListSubheader>
+      }
+      className={classes.root}
+      >
       {mappedComponents}
-    </div>
+    </List>
   )
 }
 

16c559497da22744d6a1213a29127c328b418ee6
Zmrihawy <zmrihawy@stud.ntnu.no>
2019-10-07 14:36:03 +0200

    #9 Create NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import MainPage from "./layouts/MainPage";
 import { Provider } from "react-redux";
-
+import MainBar from './components/MainBar';
 import store from "./store";
+
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <MainBar/>
         <MainPage />
       </div>
     </Provider>
--- client/src/layouts/HomePage.js
+++ client/src/layouts/HomePage.js
@@ -1,9 +1,15 @@
 import React from "react";
 import SearchBar from "../components/SearchBar.js";
+import MainBar from "../components/MainBar.js";
 
 /* HomePage layout here */
 export default class HomePage extends React.Component {
   render() {
-    return <SearchBar />;
+    return(
+      <div>
+          <SearchBar />
+          
+      </div>
+    )
   }
 }

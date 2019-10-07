147850c1090f7b7218aebf07417a14924b9320fd
Rahimu Kasibante <rahimuk@stud.ntnu.no>
2019-10-07 17:39:03 +0200

    Issue #6: Addded button to cart and updated styles

--- client/src/layouts/ItemDetails.js
+++ client/src/layouts/ItemDetails.js
@@ -1,52 +1,69 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+
 
 
 import '../styles/item-details.css';
-
 import airpods from '../media/images/items/Airpods.jpg';
 
-export default function SimpleContainer() {
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+
+
+export default function ItemDetails() {
+  const classes = useStyles();
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="lg">
         <Typography component="div" style={{ backgroundColor: 'inherit', height: '100vh', }}>
+        
+            <Paper className="_content">
+                       
+                       <div className="_item_image">
+                             <CardMedia
+                               component="img"
+                               alt="Component Image"
+                               height="100%"
+                               image={airpods}
+                               title="Image name here"
+                             />
+                           </div>
+                           
+                           <div className="_item_description">
+                             <CardContent>
+                     
+                               <Typography gutterBottom variant="h5" component="h2">
+                                 Item title here
+                               </Typography>
+                     
+                               <Typography variant="body2" color="textSecondary" component="p">
+                                 Item Description here!
+                               </Typography>
+                     
+                               <Button variant="outlined" color="primary" className={classes.button}>
+                                     Add to Basket
+                               </Button>
+                     
+                             </CardContent>
+                         </div>
+            </Paper>
+                    
+        </Typography>
           
-          <div className="_content">
-            
-            <div className="_item_image">
-                  <CardMedia
-                    component="img"
-                    alt="Component Image"
-                    height="100%"
-                    image={airpods}
-                    title="Image name here"
-                  />
-                </div>
-                
-                <div className="_item_description">
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Item title here
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                      Item Description here!
-                    </Typography>
-                  </CardContent>
-                </div>
-          
-          <p> To be styled to look better, - rahim </p>
-          
-          </div>
-          
-          
-          </Typography>
-      </Container>
     </React.Fragment>
   );
 }

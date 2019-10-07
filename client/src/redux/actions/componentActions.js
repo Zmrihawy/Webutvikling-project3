14ae4ddb1dc532166e57118deff07fc4d0fc82f9
rubensseva <r.sevaldson@gmail.com>
2019-10-07 17:12:19 +0200

    Issue #48 - Add better error logging to redux

--- client/src/redux/actions/componentActions.js
+++ client/src/redux/actions/componentActions.js
@@ -4,12 +4,21 @@ export const getComponents = () => dispatch => {
   return fetch("api/component")
     .then(res => res.json())
     .then(res => {
+      if (Array.isArray(res)) {
+        return dispatch({
+          type: GET_COMPONENTS,
+          payload: (res ? res : [])
+        });
+      } 
+      console.log("Error: components object was not an array, setting components to empty array and printing components, please inspect:");
+      console.log(res);
       return dispatch({
         type: GET_COMPONENTS,
-        payload: (res ? res : [])
+        payload: ([])
       });
     })
     .catch(err => {
+      console.log(err);
       return dispatch({
         type: GET_COMPONENTS,
         payload: []

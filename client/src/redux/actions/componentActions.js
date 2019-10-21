729930183b9e165206ecb2c4376b745aa98ba493
rubensseva <rubenss@stud.ntnu.no>
2019-10-21 14:16:33 +0200

    Issue #59 - Make featured items frontend use backend endpoint

--- client/src/redux/actions/componentActions.js
+++ client/src/redux/actions/componentActions.js
@@ -1,4 +1,4 @@
-import { GET_COMPONENTS } from "./types";
+import { GET_COMPONENTS, GET_FEATURED_COMPONENTS } from "./types";
 
 export const getComponents = () => dispatch => {
   return fetch("api/component")
@@ -27,3 +27,33 @@ export const getComponents = () => dispatch => {
       });
     });
 };
+
+
+export const getFeaturedComponents = () => dispatch => {
+  console.log("in getFeaturedComponents");
+  return fetch("api/component/featuredComponents")
+    .then(res => res.json())
+    .then(res => {
+      if (Array.isArray(res)) {
+        return dispatch({
+          type: GET_FEATURED_COMPONENTS,
+          payload: res ? res : []
+        });
+      }
+      console.log(
+        "Error: featured components object was not an array, setting components to empty array and printing components, please inspect:"
+      );
+      console.log(res);
+      return dispatch({
+        type: GET_FEATURED_COMPONENTS,
+        payload: []
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      return dispatch({
+        type: GET_FEATURED_COMPONENTS,
+        payload: []
+      });
+    });
+}
